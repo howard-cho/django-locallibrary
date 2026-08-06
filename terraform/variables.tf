30c66f371d87bd2601a5052d9a5c4af96ee574c7
Howard Cho <howardcho.info@gmail.com>
2026-08-05 17:49:57 -0700

    Continue working on cloud infra
    - Updated Terraform module with
      - Security groups with ingress/egress
      - IAM instance profiles for Route53
      - Automatically run ansible-playbook to install Nginx
    - Create initial Ansible playbook to install Nginx

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,12 +2,26 @@ variable "aws_region" {
   default = "us-west-1"
 }
 
+variable "instance_type" {
+  default = "t3.small"
+}
+
 variable "public_key_path" {
   description = "Path to your SSH public key"
   type        = string
 }
 
+variable "private_key_path" {
+  description = "Path to your SSH private key"
+  type        = string
+}
+
 variable "source_ip" {
   description = "Source public IP in CIDR notation, e.g. 1.2.3.4/32"
+  type        = string
+}
+
+variable "howardcho_zone_id" {
+  description = "Route53 zone ID from the foundation config"
   type        = string
 }
